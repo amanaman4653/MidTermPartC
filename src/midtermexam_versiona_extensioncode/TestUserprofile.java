c460b5ed62ee5026b35ae5de6a2c03eee628730a
User <User@User-PC.hitronhub.home>
2021-10-27 19:57:20 -0400

    Midterm exam

---
+++ src/midtermexam_versiona_extensioncode/TestUserprofile.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package midtermexam_versiona_extensioncode;
+
+/**
+ *
+ * @author Aman Aman
+ * 27/10/2021
+ */
+public class TestUserprofile {
+    
+        userID = givenID;
+        genre= givenGenre;
+    
+
+    /**
+     * A getter for the userID
+     * @return the userID
+     */
+    public String getUserID() {
+        return userID;
+    }
+
+    /**
+     * @param userID the userID to set
+     */
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    /**
+     * A getter for the genre
+     * @return the genre
+     */
+    public String getGenre() {
+        return genre;
+    }
+
+    /**
+     * @param genre the genre to set
+     */
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+    
+    
+    
+    
+    
+}//end class
+}
